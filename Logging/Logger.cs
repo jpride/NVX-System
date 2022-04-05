b649d3e13ff0707a77864e72993083d53017f06d
Jeff Pride <jpride@gmail.com>
2022-04-05 09:59:38 -0500

    Added Logging

---
+++ Logging/Logger.cs
@@ -0,0 +1,30 @@
+﻿using Crestron.SimplSharp;
+
+namespace TSISignageApp.Logging
+{
+	
+
+	public enum eDebugEventType
+	{
+		NA,
+		Info,
+		Notice,
+		Ok,
+		Warn,
+		Error
+	}
+
+	public class Logging
+	{
+		public static void OnDebug ( eDebugEventType eventType, string str, params object[ ] list )
+		{
+			CrestronConsole.PrintLine ( str, list );
+			switch (eventType)
+			{
+				case eDebugEventType.Notice: ErrorLog.Notice ( str, list ); break;
+				case eDebugEventType.Warn: ErrorLog.Warn ( str, list ); break;
+				case eDebugEventType.Error: ErrorLog.Error ( str, list ); break;
+			}
+		}
+	}
+}
